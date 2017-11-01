953d9165a223c777ab1b3f0d27e9913894c9c9c0
Jack Young <316802388@qq.com>
2017-11-01 11:00:27 +0800

    setup new project

---
+++ JYImageTextCombine/JYImageTextCombine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JYImageTextCombine
+//
+//  Created by Jack Young on 2017/11/1.
+//  Copyright © 2017年 Jack Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JYImageTextCombine/JYImageTextCombine/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JYImageTextCombine
+//
+//  Created by Jack Young on 2017/11/1.
+//  Copyright © 2017年 Jack Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
